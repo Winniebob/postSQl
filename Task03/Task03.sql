f3ac24c07b4aee7ee5782fe571441e138b786dc7
Winniebob <vilksmax@gmail.com>
2023-09-09 17:44:29 +0300

    Работа с несколькими таблицами

--- Task03/Task03.sql
+++ Task03/Task03.sql
@@ -24,17 +24,17 @@ GROUP BY users.id
 SELECT from_user_id, users.lastname, to_user_id,  count(messages.to_user_id) as send_messeg
 FROM messages
 	LEFT JOIN users ON users.id = messages.from_user_id
-where to_user_id = '1'
-group by from_user_id
-order by send_messeg DESC
+WHERE to_user_id = '1'
+GROUP BY from_user_id
+ORDER BY send_messeg DESC
 LIMIT 1;
 
 
 -- общее количество лайков, которые получили пользователи младше 18 лет
 SELECT likes.media_id, users.lastname, count(likes.id) 
 FROM likes
-	LEFT JOIN media ON media.id = likes.media_id
-	LEFT JOIN users ON users.id = media.user_id
+    LEFT JOIN media ON media.id = likes.media_id
+    LEFT JOIN users ON users.id = media.user_id
     LEFT JOIN profiles ON profiles.user_id = users.id 
 WHERE (YEAR(NOW())-YEAR(birthday)) < 18
 GROUP BY likes.media_id
